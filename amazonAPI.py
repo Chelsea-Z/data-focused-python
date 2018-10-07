feed4e3b5a1e23454aa0d75b0077db3e731b0d96
Chelsea-Z <43322537+Chelsea-Z@users.noreply.github.com>
2018-10-07 17:02:47 -0400

    Create amazonAPI.py

---
+++ amazonAPI.py
@@ -0,0 +1,153 @@
+import json, unirest
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B005MRMOAU",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r1 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B00AH5AOUE",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r2 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B01EVBDL5E",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r3 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B071NQ6BVC",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r4
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B00QVKYCQ6",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r5 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B005J0PWG8",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r6 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B002N6695E",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r7 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B007C68Q60",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r8 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B00KVGMSVC",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r9 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B01CG9VCUE",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r10 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B002N66904",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r11 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B076CNQMMQ",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r12 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B00BWEAHR4",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r13 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B005HQN74S",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r14= response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B00BGSJBZK",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r15 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B006GIPJFQ",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r16 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B005HQPSO0",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r17 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B005VU03D8",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r18 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B005J6MB2U",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r19 = response.body
+response = unirest.get("https://scrapehero-amazon-product-info-v1.p.mashape.com/product-details?asin=B002N669NG",
+  headers={
+    "X-Mashape-Key": "VOyoFi4U2OmsheyuGIA3lr5dye7cp1stBVjjsnSYivV8upUl6K",
+    "Accept": "application/json"
+  }
+)
+r20 = response.body
+
+
+import pandas as pd
+r = [r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20]
+rows_list = []
+keys = ['name','price','url']
+for ri in r :
+    d = {k: ri[k] for k in keys}
+    rows_list.append(d)
+    
+amazon_df = pd.DataFrame(rows_list)
+amazon_df.to_csv('AmazonAPI.csv', encoding = 'utf-8')
